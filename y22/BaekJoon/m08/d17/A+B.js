42f46e079bc1888e9250cbc7791c347a44eca0c9
taemin-Jang <jtm990506@gmail.com>
2022-08-18 16:58:56 +0900

    1000번 A+B fs로 풀었을 경우 추가

--- y22/BaekJoon/m08/d17/A+B.js
+++ y22/BaekJoon/m08/d17/A+B.js
@@ -1,34 +1,35 @@
 // 1000번 A+B
 // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
-const readline = require("readline");
+// const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let input = [];
-let sum = 0;
+// let input = [];
+// let sum = 0;
 
-rl.on("line", (line) => {
-  input = line.split(" ").map((el) => parseInt(el));
-  rl.close();
-});
+// rl.on("line", (line) => {
+//   input = line.split(" ").map((el) => parseInt(el));
+//   rl.close();
+// });
 
-rl.on("close", () => {
-  for (let i = 0; i < input.length; i++) {
-    sum += input[i];
-  }
-  console.log(sum);
+// rl.on("close", () => {
+//   for (let i = 0; i < input.length; i++) {
+//     sum += input[i];
+//   }
+//   console.log(sum);
 
-  process.exit();
-});
+//   process.exit();
+// });
 
 // fs로 풀었을 경우!
 
-// var fs = require('fs');
+var fs = require("fs");
 // var input = fs.readFileSync('/dev/stdin').toString().split(' ');
-// var a = parseInt(input[0]);
-// var b = parseInt(input[1]);
-// console.log(a + b);
+var input = fs.readFileSync(0, "utf8").toString().split(" ");
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a + b);
