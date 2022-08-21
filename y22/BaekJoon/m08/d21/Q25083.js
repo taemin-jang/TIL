41f3123c4c51f440c25c38b77c6c43b6126b682e
taemin-Jang <jtm990506@gmail.com>
2022-08-21 23:41:58 +0900

    25083번 새싹

---
+++ y22/BaekJoon/m08/d21/Q25083.js
@@ -0,0 +1,9 @@
+// 25083번 새싹
+// 아래 예제와 같이 새싹을 출력하시오.
+
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
