cb088b706603b0d23e38ff5e346e5d5391dfc217
taemin-Jang <jtm990506@gmail.com>
2022-09-06 23:53:38 +0900

    2908번 상수

--- y22/BaekJoon/m09/d06/Q2908.js
+++ y22/BaekJoon/m09/d06/Q2908.js
@@ -4,15 +4,21 @@ const file =
   process.platform === "linux" ? "/dev/stdin" : "./y22/BaekJoon/input.txt";
 
 const input = require("fs").readFileSync(file).toString().trim().split(" ");
-let answer = [];
+// let answer = [];
 
-input.forEach((v, i) => {
-  let a = v.split("");
-  v = "";
-  for (let j = 2; j >= 0; j--) {
-    v += a[j];
-  }
-  answer.push(+v);
-});
+// input.forEach((v, i) => {
+//   let a = v.split("");
+//   v = "";
+//   for (let j = 2; j >= 0; j--) {
+//     v += a[j];
+//   }
+//   answer.push(+v);
+// });
 
-answer[0] < answer[1] ? console.log(answer[1]) : console.log(answer[0]);
+// answer[0] < answer[1] ? console.log(answer[1]) : console.log(answer[0]);
+
+// 다른 풀이
+
+const [a, b] = input.map((v) => [...v].reverse().join(""));
+
+console.log(a > b ? a : b);
